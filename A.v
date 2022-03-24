ddb3a66eafc345b9056caa1db095924434fec3f3
Henry_Liu <2416224849@qq.com>
2022-03-24 12:58:59 +0800

    format code

--- A.v
+++ A.v
@@ -1,22 +1,23 @@
 module A #(
-        parameter PIXELS_IN_BATCH  = 16,
-        parameter PSAD_BIT_DEPTH = 14,
-        parameter ADDEND_BIT_DEPTH = 11
-    ) (
-        input      [PIXELS_IN_BATCH*PSAD_BIT_DEPTH-1:0] psad_ad_input ,
-        input      [PIXELS_IN_BATCH*ADDEND_BIT_DEPTH-1:0] psad_ad_addend,
-        output reg [PIXELS_IN_BATCH*PSAD_BIT_DEPTH-1:0] psad_ad_output
-    );
+    parameter PIXELS_IN_BATCH  = 16,
+    parameter PSAD_BIT_DEPTH   = 14,
+    parameter ADDEND_BIT_DEPTH = 11
+) (
+    input      [  PIXELS_IN_BATCH*PSAD_BIT_DEPTH-1:0] psad_ad_input ,
+    input      [PIXELS_IN_BATCH*ADDEND_BIT_DEPTH-1:0] psad_ad_addend,
+    output reg [  PIXELS_IN_BATCH*PSAD_BIT_DEPTH-1:0] psad_ad_output
+);
 
 
     genvar i;
     generate
         for (i = 0;i < PIXELS_IN_BATCH;i = i + 1)
-        begin
-            always @(*)
             begin
-                psad_ad_output[(i+1)*PSAD_BIT_DEPTH-1:i*PSAD_BIT_DEPTH]=psad_ad_input[(i+1)*PSAD_BIT_DEPTH-1:i*PSAD_BIT_DEPTH]+psad_ad_addend[(i+1)*ADDEND_BIT_DEPTH-1:i*ADDEND_BIT_DEPTH];
-            end
-        end
-    endgenerate
-endmodule
+                always @(*)
+                    begin
+                        psad_ad_output[(i+1)*PSAD_BIT_DEPTH-1:i*PSAD_BIT_DEPTH]
+                            = psad_ad_input[(i+1)*PSAD_BIT_DEPTH-1:i*PSAD_BIT_DEPTH]+psad_ad_addend[(i+1)*ADDEND_BIT_DEPTH-1:i*ADDEND_BIT_DEPTH];
+                    end
+                end
+        endgenerate
+    endmodule
--- FIFO_8.v
+++ FIFO_8.v
@@ -1,11 +1,11 @@
-module FIFO_8(
-input clk_i,
-input rst_n_i,
-input wire [127:0]data_in0,
-input wire [7:0]data_in1,
-output wire [127:0]data_out
+module FIFO_8 (
+    input               clk_i   ,
+    input               rst_n_i ,
+    input  wire [127:0] data_in0,
+    input  wire [  7:0] data_in1,
+    output wire [127:0] data_out
 );
 
-assign data_out = { data_in1[7:0] , data_in0[127:8] };
+    assign data_out = { data_in1[7:0] , data_in0[127:8] };
 
 endmodule
