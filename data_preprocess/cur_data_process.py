3460774e5c76b6c0033c2bae8477411bba009332
Yaotian_Liu <2416224849@qq.com>
2022-03-31 16:15:54 +0800

    fix a bug

--- data_preprocess/cur_data_process.py
+++ data_preprocess/cur_data_process.py
@@ -9,7 +9,8 @@ size = 8
 blocks = []
 for row in range(2160 // size):
     for col in range(3840 // size):
-        block = data[size * row : size * (row + 1), size * col : size * (col + 1)].T
+        block = data[size * row : size * (row + 1), size * col : size * (col + 1)]
+        block = np.rot90(block)
         blocks.append(block)
 
 blocks = np.array(blocks)
