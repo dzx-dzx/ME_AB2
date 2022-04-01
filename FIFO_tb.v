d556d884c7ee5c37241cdaa5bd8b91e46aa6f8b7
ZzX1ng <96902567+ZzX1ng@users.noreply.github.com>
2022-04-01 13:15:16 +0800

    Update FIFO_tb.v

--- FIFO_tb.v
+++ FIFO_tb.v
@@ -35,8 +35,6 @@ initial begin
         $dumpfile("FIFO.vcd");
         $dumpvars(0, FIFO_tb);
         data_in={8'hda,8'h2e,8'hca,8'h45,8'h7f,8'hce,8'h8a,8'h96,8'hc8,8'h06,8'h47,8'h18,8'h20,8'h4e,8'hc2,8'h75,8'h08,8'h37,8'h87,8'hab,8'h67,8'hac,8'h3b};
-        rst_n_i = 0;
-        #6 rst_n_i=1;
         #500 $finish;
     end
 end
