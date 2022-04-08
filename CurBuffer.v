2c29d25b0e046938ede85b67e55d54bc8bdd8255
Yaotian_Liu <2416224849@qq.com>
2022-04-08 11:31:10 +0800

    fix blocking assignments

--- CurBuffer.v
+++ CurBuffer.v
@@ -33,10 +33,6 @@ module CurBuffer (
     assign cur_out = {out_row_8, out_row_7, out_row_6, out_row_5,
         out_row_4, out_row_3, out_row_2, out_row_1};
 
-
-
-
-
     always @ (posedge clk) begin
         if (rst) begin
             buffer_0 <= 0;
@@ -45,73 +41,73 @@ module CurBuffer (
         else  case (half)
             // buffer_0 is valid and write buffer_1
             1'b0 : begin
-                buffer_1[addr+0]  = cur_in[0];
-                buffer_1[addr+1]  = cur_in[1];
-                buffer_1[addr+2]  = cur_in[2];
-                buffer_1[addr+3]  = cur_in[3];
-                buffer_1[addr+4]  = cur_in[4];
-                buffer_1[addr+5]  = cur_in[5];
-                buffer_1[addr+6]  = cur_in[6];
-                buffer_1[addr+7]  = cur_in[7];
-                buffer_1[addr+8]  = cur_in[8];
-                buffer_1[addr+9]  = cur_in[9];
-                buffer_1[addr+10] = cur_in[10];
-                buffer_1[addr+11] = cur_in[11];
-                buffer_1[addr+12] = cur_in[12];
-                buffer_1[addr+13] = cur_in[13];
-                buffer_1[addr+14] = cur_in[14];
-                buffer_1[addr+15] = cur_in[15];
-                buffer_1[addr+16] = cur_in[16];
-                buffer_1[addr+17] = cur_in[17];
-                buffer_1[addr+18] = cur_in[18];
-                buffer_1[addr+19] = cur_in[19];
-                buffer_1[addr+20] = cur_in[20];
-                buffer_1[addr+21] = cur_in[21];
-                buffer_1[addr+22] = cur_in[22];
-                buffer_1[addr+23] = cur_in[23];
-                buffer_1[addr+24] = cur_in[24];
-                buffer_1[addr+25] = cur_in[25];
-                buffer_1[addr+26] = cur_in[26];
-                buffer_1[addr+27] = cur_in[27];
-                buffer_1[addr+28] = cur_in[28];
-                buffer_1[addr+29] = cur_in[29];
-                buffer_1[addr+30] = cur_in[30];
-                buffer_1[addr+31] = cur_in[31];
+                buffer_1[addr+0]  <= cur_in[0];
+                buffer_1[addr+1]  <= cur_in[1];
+                buffer_1[addr+2]  <= cur_in[2];
+                buffer_1[addr+3]  <= cur_in[3];
+                buffer_1[addr+4]  <= cur_in[4];
+                buffer_1[addr+5]  <= cur_in[5];
+                buffer_1[addr+6]  <= cur_in[6];
+                buffer_1[addr+7]  <= cur_in[7];
+                buffer_1[addr+8]  <= cur_in[8];
+                buffer_1[addr+9]  <= cur_in[9];
+                buffer_1[addr+10] <= cur_in[10];
+                buffer_1[addr+11] <= cur_in[11];
+                buffer_1[addr+12] <= cur_in[12];
+                buffer_1[addr+13] <= cur_in[13];
+                buffer_1[addr+14] <= cur_in[14];
+                buffer_1[addr+15] <= cur_in[15];
+                buffer_1[addr+16] <= cur_in[16];
+                buffer_1[addr+17] <= cur_in[17];
+                buffer_1[addr+18] <= cur_in[18];
+                buffer_1[addr+19] <= cur_in[19];
+                buffer_1[addr+20] <= cur_in[20];
+                buffer_1[addr+21] <= cur_in[21];
+                buffer_1[addr+22] <= cur_in[22];
+                buffer_1[addr+23] <= cur_in[23];
+                buffer_1[addr+24] <= cur_in[24];
+                buffer_1[addr+25] <= cur_in[25];
+                buffer_1[addr+26] <= cur_in[26];
+                buffer_1[addr+27] <= cur_in[27];
+                buffer_1[addr+28] <= cur_in[28];
+                buffer_1[addr+29] <= cur_in[29];
+                buffer_1[addr+30] <= cur_in[30];
+                buffer_1[addr+31] <= cur_in[31];
             end
             // buffer_1 is valid and write buffer_0
             1'b1 : begin
-                buffer_0[addr+0]  = cur_in[0];
-                buffer_0[addr+1]  = cur_in[1];
-                buffer_0[addr+2]  = cur_in[2];
-                buffer_0[addr+3]  = cur_in[3];
-                buffer_0[addr+4]  = cur_in[4];
-                buffer_0[addr+5]  = cur_in[5];
-                buffer_0[addr+6]  = cur_in[6];
-                buffer_0[addr+7]  = cur_in[7];
-                buffer_0[addr+8]  = cur_in[8];
-                buffer_0[addr+9]  = cur_in[9];
-                buffer_0[addr+10] = cur_in[10];
-                buffer_0[addr+11] = cur_in[11];
-                buffer_0[addr+12] = cur_in[12];
-                buffer_0[addr+13] = cur_in[13];
-                buffer_0[addr+14] = cur_in[14];
-                buffer_0[addr+15] = cur_in[15];
-                buffer_0[addr+16] = cur_in[16];
-                buffer_0[addr+17] = cur_in[17];
-                buffer_0[addr+18] = cur_in[18];
-                buffer_0[addr+19] = cur_in[19];
-                buffer_0[addr+20] = cur_in[20];
-                buffer_0[addr+21] = cur_in[21];
-                buffer_0[addr+22] = cur_in[22];
-                buffer_0[addr+23] = cur_in[23];
-                buffer_0[addr+24] = cur_in[24];
-                buffer_0[addr+25] = cur_in[25];
-                buffer_0[addr+26] = cur_in[26];
-                buffer_0[addr+27] = cur_in[27];
-                buffer_0[addr+28] = cur_in[28];
-                buffer_0[addr+29] = cur_in[29];
-                buffer_0[addr+30] = cur_in[30];
-                buffer_0[addr+31] = cur_in[31];
+                buffer_0[addr+0]  <= cur_in[0];
+                buffer_0[addr+1]  <= cur_in[1];
+                buffer_0[addr+2]  <= cur_in[2];
+                buffer_0[addr+3]  <= cur_in[3];
+                buffer_0[addr+4]  <= cur_in[4];
+                buffer_0[addr+5]  <= cur_in[5];
+                buffer_0[addr+6]  <= cur_in[6];
+                buffer_0[addr+7]  <= cur_in[7];
+                buffer_0[addr+8]  <= cur_in[8];
+                buffer_0[addr+9]  <= cur_in[9];
+                buffer_0[addr+10] <= cur_in[10];
+                buffer_0[addr+11] <= cur_in[11];
+                buffer_0[addr+12] <= cur_in[12];
+                buffer_0[addr+13] <= cur_in[13];
+                buffer_0[addr+14] <= cur_in[14];
+                buffer_0[addr+15] <= cur_in[15];
+                buffer_0[addr+16] <= cur_in[16];
+                buffer_0[addr+17] <= cur_in[17];
+                buffer_0[addr+18] <= cur_in[18];
+                buffer_0[addr+19] <= cur_in[19];
+                buffer_0[addr+20] <= cur_in[20];
+                buffer_0[addr+21] <= cur_in[21];
+                buffer_0[addr+22] <= cur_in[22];
+                buffer_0[addr+23] <= cur_in[23];
+                buffer_0[addr+24] <= cur_in[24];
+                buffer_0[addr+25] <= cur_in[25];
+                buffer_0[addr+26] <= cur_in[26];
+                buffer_0[addr+27] <= cur_in[27];
+                buffer_0[addr+28] <= cur_in[28];
+                buffer_0[addr+29] <= cur_in[29];
+                buffer_0[addr+30] <= cur_in[30];
+                buffer_0[addr+31] <= cur_in[31];
             end
 
         endcase
@@ -304,7 +300,7 @@ module CurBuffer (
                 end
                 default :
                     {out_row_8, out_row_7, out_row_6, out_row_5,
-                        out_row_4, out_row_3, out_row_2, out_row_1} = 0;
+                        out_row_4, out_row_3, out_row_2, out_row_1} <= 0;
             endcase
         end
         // Not at inter
