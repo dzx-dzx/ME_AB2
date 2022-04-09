c3acfdbe628c943096d846ea6a15708cf89035fa
Yaotian_Liu <2416224849@qq.com>
2022-04-09 17:30:41 +0800

    update RefSRAM

--- RefSRAM.v
+++ RefSRAM.v
@@ -1,21 +1,19 @@
-`timescale 1 ns / 1 ns
 // Cold boot requires 69 cycles
 
 
 module RefSRAM (
-    input               clk       ,
-    input               rst       ,
-    input  wire [ 63:0] ref_in    , // 8 pixels
-    output wire         read_en   ,
-    output reg  [183:0] ref_out   , // 23 pixels
-    output reg          sram_ready, // Set high when ref_out is valid.
-    output reg          next_block  // Set high when CurBuffer need to past the next block
+    input               clk         ,
+    input               rst         ,
+    input               en          ,
+    input  wire [ 63:0] ref_in      , // 8 pixels
+    output reg  [183:0] ref_out     , // 23 pixels
+    output reg  [ 31:0] ref_mem_addr,
+    output reg          sram_ready  , // Set high when ref_out is valid.
+    output reg          next_block    // Set high when CurBuffer need to past the next block
 );
 
     reg [4:0] addr           ;
     reg [3:0] sram_is_written; // Which sram is at WRITE state
-
-    assign read_en = sram_is_written != 4'b0000;
 
     wire [63:0] q_1;
     wire [63:0] q_2;
@@ -36,7 +34,7 @@ module RefSRAM (
     wire       test;
     wire       rme ;
     wire [3:0] rm  ;
-    assign me   = 1;
+    assign me   = en;
     assign test = 0;
     assign rme  = 0;
     assign rm   = 4'b0000;
@@ -46,31 +44,34 @@ module RefSRAM (
 
 
     always @(*) begin
-        if (addr != 0)
-            case (sram_is_written)
-                4'b0001 :
-                    ref_out <= {q_2, q_3, q_4[63:8]};
-                4'b0010 :
-                    ref_out <= {q_3, q_4, q_1[63:8]};
-                4'b0100 :
-                    ref_out <= {q_4, q_1, q_2[63:8]};
-                4'b1000 :
-                    ref_out <= {q_1, q_2, q_3[63:8]};
-                default :
-                    ref_out <= 0;
-            endcase
-        else
-            case (sram_is_written)
-                4'b0001 :
-                    ref_out <= {q_1, q_2, q_3[63:8]};
-                4'b0010 :
-                    ref_out <= {q_2, q_3, q_4[63:8]};
-                4'b0100 :
-                    ref_out <= {q_3, q_4, q_1[63:8]};
-                4'b1000 :
-                    ref_out <= {q_4, q_1, q_2[63:8]};
-                default : ref_out <= 0;
-            endcase
+        if (en) begin
+            if (addr != 0)
+                case (sram_is_written)
+                    4'b0001 :
+                        ref_out <= {q_2, q_3, q_4[63:8]};
+                    4'b0010 :
+                        ref_out <= {q_3, q_4, q_1[63:8]};
+                    4'b0100 :
+                        ref_out <= {q_4, q_1, q_2[63:8]};
+                    4'b1000 :
+                        ref_out <= {q_1, q_2, q_3[63:8]};
+                    default :
+                        ref_out <= 0;
+                endcase
+            else
+                case (sram_is_written)
+                    4'b0001 :
+                        ref_out <= {q_1, q_2, q_3[63:8]};
+                    4'b0010 :
+                        ref_out <= {q_2, q_3, q_4[63:8]};
+                    4'b0100 :
+                        ref_out <= {q_3, q_4, q_1[63:8]};
+                    4'b1000 :
+                        ref_out <= {q_4, q_1, q_2[63:8]};
+                    default : ref_out <= 0;
+                endcase
+        end
+        else ref_out <= 0;
     end
 
     // All SRAM share the same address
@@ -81,7 +82,7 @@ module RefSRAM (
             block_cnt       <= 0;
             next_line       <= 0;
         end
-        else begin
+        else if (en) begin
             // entry to next block
             if (addr == 5'd22) begin
                 addr <= 0;
@@ -106,13 +107,23 @@ module RefSRAM (
         end
     end
 
+    // mem_addr
+    always @(posedge clk) begin
+        if(rst)
+            ref_mem_addr <= 0;
+        else if (en && sram_is_written != 4'b0000)
+            ref_mem_addr <= ref_mem_addr + 8;
+    end
+
     // Next block for CurBuffer
     always @(posedge clk) begin
         if (rst)
             next_block <= 0;
-        else if (sram_ready && addr == 8)
-            next_block <= 1;
-        else next_block <= 0;
+        else if(en) begin
+            if (sram_ready && addr == 8)
+                next_block <= 1;
+            else next_block <= 0;
+        end
     end
 
     // First time write sram_4 => Sram is ready
@@ -120,14 +131,16 @@ module RefSRAM (
         if (rst) begin
             sram_ready <= 0;
         end
-        else if (next_line) begin
-            sram_ready <= 0;
+        else if (en) begin
+            if (next_line) begin
+                sram_ready <= 0;
+            end
+            else if (sram_ready == 0 && sram_is_written == 4'b1000) begin
+                sram_ready <= 1;
+            end
+            else
+                sram_ready <= sram_ready;
         end
-        else if (sram_ready == 0 && sram_is_written == 4'b1000) begin
-            sram_ready <= 1;
-        end
-        else
-            sram_ready <= sram_ready;
     end
 
 
--- RefSRAM_tb.v
+++ RefSRAM_tb.v
@@ -7,67 +7,29 @@ module RefSRAM_tb ();
 // Ports
     reg          clk        = 0;
     reg          rst           ;
-    reg  [ 63:0] ref_in        ;
+    reg          en            ;
+    wire [ 63:0] ref_in        ;
     wire [183:0] ref_out       ;
     wire         sram_ready    ;
     wire         next_block    ;
-    wire         read_en       ;
 
-    reg [ 7:0] mem     [0:7000];
-    reg [11:0] mem_addr        ;
+    wire [31:0] ref_mem_addr;
 
-    initial begin
-        $readmemh("./data_preprocess/data/ref_test.txt", mem);
-    end
+    ref_mem U_ref_mem (
+        .en      (en          ),
+        .addr    (ref_mem_addr),
+        .ref_data(ref_in      )
+    );
 
-
-    // always @(posedge clk) begin
-    //     if (rst) begin
-    //         mem_addr <= 0;
-    //         ref_in   <= 0;
-    //     end
-    //     else if(read_en) begin
-    //         ref_in[7:0]   <= mem[mem_addr + 0];
-    //         ref_in[15:8]  <= mem[mem_addr + 1];
-    //         ref_in[23:16] <= mem[mem_addr + 2];
-    //         ref_in[31:24] <= mem[mem_addr + 3];
-    //         ref_in[39:32] <= mem[mem_addr + 4];
-    //         ref_in[47:40] <= mem[mem_addr + 5];
-    //         ref_in[55:48] <= mem[mem_addr + 6];
-    //         ref_in[63:56] <= mem[mem_addr + 7];
-
-    //         mem_addr <= mem_addr + 8;
-    //     end
-    //     else ;
-    // end
-
-    always @(posedge clk) begin
-        if (rst) begin
-            mem_addr <= 0;
-        end
-        else begin
-            mem_addr <= mem_addr + 8;
-        end
-    end
-
-    genvar i;
-
-    generate
-        for (i = 0; i < 8; i = i + 1)
-            always @(*) begin
-                if (!rst)
-                    ref_in[8*i+7:(i)*8] <= mem[mem_addr+i];
-            end
-    endgenerate
-
-    RefSRAM RefSRAM_dut (
-        .clk       (clk       ),
-        .rst       (rst       ),
-        .ref_in    (ref_in    ),
-        .read_en   (read_en   ),
-        .ref_out   (ref_out   ),
-        .sram_ready(sram_ready),
-        .next_block(next_block)
+    RefSRAM U_RefSRAM (
+        .clk         (clk         ),
+        .rst         (rst         ),
+        .en          (en          ),
+        .ref_in      (ref_in      ),
+        .ref_out     (ref_out     ),
+        .ref_mem_addr(ref_mem_addr),
+        .sram_ready  (sram_ready  ),
+        .next_block  (next_block  )
     );
 
     initial begin
@@ -79,7 +41,9 @@ module RefSRAM_tb ();
     initial begin
         clk = 0;
         rst = 1;
+        en = 0;
         #20 rst = 0;
+        #20 en = 1;
     end
 
     always
