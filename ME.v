90b402bb15ed04038ac5c1a16aec32465e88c45c
dzx-dzx <dzx_dzx@hotmail.com>
2022-04-16 17:34:26 +0800

    Correct the time cycle.

--- ME.v
+++ ME.v
@@ -177,7 +177,7 @@ module ME #(
     wire       MSAD_data_processing;
     wire [4:0] current_row         ;
     TIMER #(
-        .INTERIM_CYCLE   (16),
+        .INTERIM_CYCLE   (15),
         .FULL_CYCLE      (23),
         .OUTPUT_UP_PERIOD(16)
     ) timer (
