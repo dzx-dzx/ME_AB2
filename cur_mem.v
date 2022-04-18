7e82ad786f8b8c1ef08af8adb00bd66c2aada022
Yaotian_Liu <2416224849@qq.com>
2022-04-18 13:26:22 +0800

    use blocking in mem read

--- cur_mem.v
+++ cur_mem.v
@@ -30,14 +30,14 @@ module cur_mem (
     initial
         $readmemh("./data_preprocess/data/cur_test.txt", cur_mem);
 
-    always @(addr, en) begin
+    always @(*) begin
         if (en == 0)
-            cur_data <= 0;
+            cur_data = 0;
         else begin
-            cur_data[7:0]   <= cur_mem[addr];
-            cur_data[15:8]  <= cur_mem[addr + 1];
-            cur_data[23:16] <= cur_mem[addr + 2];
-            cur_data[31:24] <= cur_mem[addr + 3];
+            cur_data[7:0]   = cur_mem[addr];
+            cur_data[15:8]  = cur_mem[addr + 1];
+            cur_data[23:16] = cur_mem[addr + 2];
+            cur_data[31:24] = cur_mem[addr + 3];
         end
     end
 
--- ref_mem.v
+++ ref_mem.v
@@ -30,18 +30,18 @@ module ref_mem (
     initial
         $readmemh("./data_preprocess/data/ref_test.txt", ref_mem);
 
-    always @(addr, en) begin
+    always @(*) begin
         if (en == 0)
-            ref_data <= 0;
+            ref_data = 0;
         else begin
-            ref_data[7:0]   <= ref_mem[addr + 7];
-            ref_data[15:8]  <= ref_mem[addr + 6];
-            ref_data[23:16] <= ref_mem[addr + 5];
-            ref_data[31:24] <= ref_mem[addr + 4];
-            ref_data[39:32] <= ref_mem[addr + 3];
-            ref_data[47:40] <= ref_mem[addr + 2];
-            ref_data[55:48] <= ref_mem[addr + 1];
-            ref_data[63:56] <= ref_mem[addr];
+            ref_data[7:0]   = ref_mem[addr + 7];
+            ref_data[15:8]  = ref_mem[addr + 6];
+            ref_data[23:16] = ref_mem[addr + 5];
+            ref_data[31:24] = ref_mem[addr + 4];
+            ref_data[39:32] = ref_mem[addr + 3];
+            ref_data[47:40] = ref_mem[addr + 2];
+            ref_data[55:48] = ref_mem[addr + 1];
+            ref_data[63:56] = ref_mem[addr];
         end
     end
 
